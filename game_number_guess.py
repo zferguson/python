924ab7f111859c4c15ebb9ecb4a14595d34dd13a
zferguson <zacharysferguson@gmail.com>
2021-10-31 12:14:09 -0400

    init

--- game_number_guess.py
+++ game_number_guess.py
@@ -1,5 +1,9 @@
+# Game to guess number between minNum and maxNum
+
+# import
 import random as rd
 
+# introduciton and set variables
 minNum = 1
 maxNum = 100
 
@@ -10,6 +14,7 @@ print("Let's play a number guessing game!\n")
 userGuess = 0
 triesLeft = int(input("How many guesses would you like? "))
 
+# game loop
 while userGuess != hiddenNumber:
 	userGuess = int(input("\nGuess a number between {0} and {1}: ".format(minNum,maxNum)))
 	if triesLeft > 0:
@@ -22,7 +27,6 @@ while userGuess != hiddenNumber:
 		else:
 			print("\nThat's right!")
 	else:
-		print()
 		print("\nNo more tries, sorry! The answer was {0}.".format(hiddenNumber))
 		break
 	
